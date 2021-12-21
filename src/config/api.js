08cbc1a93b4b06a4fc3f9c7a17306f3334a44704
elton0022 <eltonrodrigues0022.er@gmail.com>
2021-12-21 08:09:09 -0300

    fix: correction in api

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@ import { api_url } from '../../env';
 const prod = "https://townsperson-api.herokuapp.com"
 
 export const api = axios.create({
-    baseURL: api_url || prod
+    baseURL: api_url? api_url : prod
 });
 
 export const apiViaCEP = axios.create({
